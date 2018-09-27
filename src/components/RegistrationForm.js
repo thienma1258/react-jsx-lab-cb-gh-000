ae1fc3beae4a696c788765bb80f117df5caa74bf
Ngoc Dong Pham <cpud1258@gmail.com>
2018-09-27 03:34:06 +0000

    automatically backed up by learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,12 @@
+import  React from 'react';
+
+export default RegistrationForm extends React.Component{
+  render(){
+      return (
+      <form>
+<input type="text" />   
+<input type="password" />
+<input type="submit"/>  
+  </form>
+      );
+}
