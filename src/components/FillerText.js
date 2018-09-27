c2d7268c8eb52271df9ad11a6110f1b839ece3bf
Ngoc Dong Pham <cpud1258@gmail.com>
2018-09-27 03:35:04 +0000

    automatically backed up by learn

---
+++ src/components/FillerText.js
@@ -0,0 +1,8 @@
+import  React from 'react';
+
+export default FillerText extends React.Component{
+  render(){
+      return (
+    <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
+      );
+}
