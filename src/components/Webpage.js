2bc956009d39d52645a7ce20cc1995864a1c0dee
Ngoc Dong Pham <cpud1258@gmail.com>
2018-09-27 03:41:39 +0000

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,12 @@
+import  React from 'react';
+import FillerText from './FillerText';
+export default class Webpage extends React.Component{
+  render(){
+      return (
+    <div>The world's coolest webpage.
+    <FillerText/>
+    <FillerText/>
+    </div>
+      );
+}
+}
